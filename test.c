adea5073609f92fb84a60e7d3faaf6c901552212
demo1 <deepahiremath20102006>
2025-08-29 12:22:52 +0530

    bacsic code

--- test.c
+++ test.c
@@ -1,13 +1,64 @@
 # include <stdio.h>
 
 int main() {
-    // int number;
-    // printf("Hello, World dfgvcvxbc vcfhbg!\n");
-    // scanf("%d", &number);
-    // printf("You entered: %d\n", number);
-    // return 0;
+
+// simple interest----
+    //  int p,r,t;
+    //  printf("enter the values for calcuating the simple:\n");
+    //  printf("principal=");
+    //  scanf("%d",&p);
+    //  printf("rate=");
+    //  scanf("%d",&r);
+    //  printf("time=");
+    //  scanf("%d",&t);
+    //  int cal=p*r*t;
+    //  printf("simple interest =%d",cal);
 
 
+
+
+//calculate maximum and minmumm of two no
+
+    // int a,b;
+    // printf("enter the no.:\n");
+    // scanf("%d %d",&a,&b);
+    // if(a>b){
+    //     printf("%d is a maximum no.",a);
+    //     printf("%d is a minmum no.",b);
+    // }
+    // else {
+    //     printf("%d is a maximum no.",b);
+    //     printf("\n%d is a minmum no.",a);
+
+
+    // }
+    
+
+
+
+// eligible or not
+    // int age;
+    // printf("enterd your age=");
+    // scanf("%d",&age);
+    // if(age>=18){
+    //     printf("you can get a drving lincense");
+    // }
+    // else{
+    //     printf("you not get a drving lincense");
+    // }
+
+
+
+// factorial
+    int n,res=1;
+    printf("enter the no.");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+         res=res*i;
+    }
+    printf("factorial of %d no. is= %d",n,res);
+
+     
 
 
 
