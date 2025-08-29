3f41b44857f74b6a7decf5dc6f9d3a0ab28875a7
demo1 <deepahiremath20102006>
2025-08-29 10:24:43 +0530

    first set

---
+++ test.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+
+int main() {
+    // int number;
+    // printf("Hello, World dfgvcvxbc vcfhbg!\n");
+    // scanf("%d", &number);
+    // printf("You entered: %d\n", number);
+    // return 0;
+
+
+
+
+
+    return 0;
+}
